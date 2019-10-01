82aecf91ffb444bcef5a513811f1f1f2679c57da
karatehcops <contact@lazers.tv>
2019-10-01 09:40:26 -0400

    Add babel config support to project-scripts lib.

--- packages/project-scripts/index.js
+++ packages/project-scripts/index.js
@@ -4,8 +4,10 @@ const path = require('path');
 
 const eslintPath = path.join(__dirname, 'dist', 'eslintrc');
 const prettierPath = path.join(__dirname, 'dist', 'prettierrc');
+const babelConfigPath = path.join(__dirname, 'babel.config.js');
 
 module.exports = {
   eslint: require(eslintPath),
   prettier: require(prettierPath),
+  babelConfig: require(babelConfigPath),
 };
