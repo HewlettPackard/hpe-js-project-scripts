cd80b580783c32f41cef01b3fecb0caddadcdbb9
Daniel Martin <daniel.l.martin@hpe.com>
2022-09-07 23:48:53 -0500

    Don't require babel config file

--- packages/project-scripts/src/config/eslintrc.js
+++ packages/project-scripts/src/config/eslintrc.js
@@ -3,7 +3,11 @@
 module.exports = {
   parser: '@babel/eslint-parser',
   parserOptions: {
+    requireConfigFile: false,
     babelOptions: {
+      babelrc: false,
+      configFile: false,
+      // your babel options
       presets: ["@babel/preset-env"],
     },
   },
